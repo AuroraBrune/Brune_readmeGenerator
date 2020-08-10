1348d0ce24b5994ab65a108d5b32fa7574923420
Aurora Brune <akbrune@hotmail.com>
2020-08-10 12:50:21 -0500

    generate markdown page done.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,32 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = data => {
   return `# ${data.title}
+
+  ![License: ${(data.lic) ? data.lic : 'None'}](https://img.shields.io/badge/License-${(data.lic) ? data.lic : 'None'})
+
+  _Repo by ${data.name}_
+
+  _${data.description}_
+
+  _Table of Contents_
+
+  _Installation:_
+  ${data.installation}
+
+  _Usage:_
+  ${data.usage}
+
+  _Contributors:_
+  ${data.contributors}
+
+  _Contributors:_
+  ${data.contributors}
+
+  _Tests:_
+  ${data.tests}
+
+  _Contact:_
+  ${data.contact}
 
 `;
 }
