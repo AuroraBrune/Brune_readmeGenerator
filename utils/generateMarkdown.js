bd8a53eac25e9858de14951b75240741330304e7
Aurora Brune <akbrune@hotmail.com>
2020-08-09 19:42:48 -0500

    prompt questions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,14 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+          # ${data.description}
+          # ${data.tableOfContents}
+          # ${data.installation}
+          # ${data.usage}
+          # ${data.license}
+          # ${data.contributors}
+          # ${data.tests}
+          # ${data.contact}
 `;
 }
 
